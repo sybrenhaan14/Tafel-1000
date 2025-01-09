9402296556215faa04ac5053f71090f6c60cc5c8
bastiaanroest <180518443+bastiaanroest@users.noreply.github.com>
2025-01-09 11:30:18 +0100

    csv file aanpassing

--- python/classes.py
+++ python/classes.py
@@ -21,36 +21,13 @@ class Verbinding:
         self.station2 = station2
         self.tijd = tijd
 
-verbindingen = [
-    Verbinding("Alkmaar","Hoorn",24),
-    Verbinding("Alkmaar", "Den Helder", 36),
-    Verbinding("Amsterdam Amstel", "Amsterdam Zuid", 10),
-    Verbinding("Amsterdam Amstel", "Amsterdam Centraal", 8),
-    Verbinding("Amsterdam Centraal", "Amsterdam Sloterdijk", 6),
-    Verbinding("Amsterdam Sloterdijk", "Haarlem", 11),
-    Verbinding("Amsterdam Sloterdijk", "Zaandam", 6),
-    Verbinding("Amsterdam Zuid","Amsterdam Sloterdijk", 16),
-    Verbinding("Amsterdam Zuid", "Schiphol Airport", 6),
-    Verbinding("Beverwijk", "Castricum", 13),
-    Verbinding("Castricum", "Alkmaar", 9),
-    Verbinding("Delft", "Den Haag Centraal", 13),
-    Verbinding("Den Haag Centraal", "Gouda", 18),
-    Verbinding("Den Haag Centraal", "Leiden Centraal", 12),
-    Verbinding("Dordrecht", "Rotterdam Centraal", 17),
-    Verbinding("Gouda", "Alphen a/d Rijn", 19),
-    Verbinding("Haarlem", "Beverwijk", 16),
-    Verbinding("Heemstede-Aerdenhout", "Haarlem", 6),
-    Verbinding("Leiden Centraal", "Heemstede-Aerdenhout", 13),
-    Verbinding("Leiden Centraal", "Alphen a/d Rijn", 14),
-    Verbinding("Leiden Centraal", "Schiphol Airport", 15),
-    Verbinding("Rotterdam Alexander", "Gouda", 10),
-    Verbinding("Rotterdam Centraal", "Schiedam Centrum", 5),
-    Verbinding("Rotterdam Centraal","Rotterdam Alexander", 8),
-    Verbinding("Schiedam Centrum", "Delft", 7),
-    Verbinding("Zaandam", "Castricum", 12),
-    Verbinding("Zaandam", "Beverwijk", 25),
-    Verbinding("Zaandam", "Hoorn", 26)
-]
+verbindingen = []
+
+with open('../csv_files/ConnectiesHolland.csv', 'r', encoding="utf-8") as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            verbinding = Verbinding(row['station1'], row['station2'], row['tijd'])
+            verbindingen.append(verbinding)
 
 class Traject:
     def __init__(self, traject_id, totale_tijd):
