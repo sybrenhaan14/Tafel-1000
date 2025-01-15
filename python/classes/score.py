bc44af3fc10499d5d0a257c736afc069099c77fe
bastiaanroest <180518443+bastiaanroest@users.noreply.github.com>
2025-01-15 14:47:54 +0100

    trajecten alleen berekenen bij >0

--- python/classes/score.py
+++ python/classes/score.py
@@ -1,7 +1,7 @@
 class Score:
     def __init__(self, netwerk):
         self.netwerk = netwerk
-        self.T = len(self.netwerk.netwerk)  
+        self.T = 0  
         self.Min = 0  
         self.p = 1  
         self.score = 1  
@@ -9,6 +9,8 @@ class Score:
     def bereken_score(self):
         
         self.Min = sum(verbinding.tijd for traject in self.netwerk.netwerk for verbinding in traject.traject)
+
+        self.T = sum(1 for traject in self.netwerk.netwerk if traject.bereken_totale_tijd() > 0)
 
         
         self.score = 10000 - (self.T * 100 + self.Min)
