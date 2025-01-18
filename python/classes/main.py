f4b2ce7d090ac91fd29a8378f65d9e3c7e8b42c0
sybrenhaan14 <180518395+sybrenhaan14@users.noreply.github.com>
2025-01-18 11:53:44 +0100

    main class gemaakt

--- python/classes/main.py
+++ python/classes/main.py
@@ -9,54 +9,55 @@ from score import *
 import os 
 
 import csv
-
-def main():
-    set_stations = Stations('../../Data/StationsHolland.csv')
-    verbindingen_lijst = Verbindingen()
-
-    netwerken = Netwerken(set_stations, verbindingen_lijst)
-    netwerk = netwerken.genereer_trajecten()
-
-    
-    output_data = []
-
-    
-    output_data.append(["train", "stations"])
-
-    # Genereer output per traject
-    for traject in netwerk.netwerk:
-        output_data.append([f'train_{traject.traject_id}', f'[{", ".join(traject.bezochte_stations)}]'])
-
-    score_calculator = Score(netwerk)
-    score = score_calculator.bereken_score()
-    output_data.append([f'score', score])
-
-    
-    return output_data, score 
+class Main:
+    def __init__(self):
+        self.simulatie()
 
 
+    def main(self):
+        set_stations = Stations('../../Data/StationsHolland.csv')
+        verbindingen_lijst = Verbindingen()
+
+        netwerken = Netwerken(set_stations, verbindingen_lijst)
+        netwerk = netwerken.genereer_trajecten()
+
+        
+        output_data = []
+
+        
+        output_data.append(["train", "stations"])
+
+        # Genereer output per traject
+        for traject in netwerk.netwerk:
+            output_data.append([f'train_{traject.traject_id}', f'[{", ".join(traject.bezochte_stations)}]'])
+
+        score_calculator = Score(netwerk)
+        score = score_calculator.bereken_score()
+        output_data.append([f'score', score])
+
+        
+        return output_data, score 
+
+    def simulatie(self):
+        
+        output_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..', 'Data', 'extra_outputs')
+        count = 0
+        
+        for count in range(1, 10):  
+            print(count)
+            count =+ 1
+            data, score = self.main()
+
+            if data:
+                
+                output_file = os.path.join(output_dir, f"holland_{score}.csv")
+
+                
+                with open(output_file, mode="w", newline="", encoding="utf-8") as file:
+                    writer = csv.writer(file)
+                    writer.writerows(data)
+            else:
+                print(f"Geen data om naar het bestand te schrijven voor simulatie {count}")
 
 
-def simulatie():
-    
-    output_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..', 'Data', 'extra_outputs')
-    count = 0
-    
-    for count in range(1, 50000):  
-        print(count)
-        count =+ 1
-        data, score = main()
-
-        if data:
-            
-            output_file = os.path.join(output_dir, f"holland_{score}.csv")
-
-            
-            with open(output_file, mode="w", newline="", encoding="utf-8") as file:
-                writer = csv.writer(file)
-                writer.writerows(data)
-        else:
-            print(f"Geen data om naar het bestand te schrijven voor simulatie {count}")
-
-
-simulatie()
+Main()
