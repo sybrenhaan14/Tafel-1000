f831c92b28dc6326e23b0da8f4f0d603104dfb95
bastiaanroest <180518443+bastiaanroest@users.noreply.github.com>
2025-01-20 11:35:44 +0100

    save vergeten

--- code/classes/netwerken.py
+++ code/classes/netwerken.py
@@ -83,3 +83,16 @@ class Netwerken:
         niet_bezochte_verbindingen = alle_verbindingen - bezochte_verbindingen
 
         return niet_bezochte_verbindingen
+    
+class Score:
+    def __init__(self, netwerk):
+        self.netwerk = netwerk
+
+    def bereken_score(self):
+        aantal_bereden_verbindingen = len(self.netwerk.netwerk)
+        aantal_trajecten = len(self.netwerk.netwerk)
+        Min = sum(verbinding.tijd for traject in self.netwerk.netwerk for verbinding in traject.traject)
+
+        score = (aantal_bereden_verbindingen / 28 ) * 10000 - (aantal_trajecten * 100 + Min)
+
+        return score
