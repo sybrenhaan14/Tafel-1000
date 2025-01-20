9c86aa68ec02e2eda3c677e16a175720eabe6a3e
sybrenhaan14 <180518395+sybrenhaan14@users.noreply.github.com>
2025-01-20 12:06:26 +0100

    station class maken en stations herschrijven

--- code/classes/stations.py
+++ code/classes/stations.py
@@ -1,19 +1,36 @@
 import csv
 import random 
+from verbindingen import *
+
+class Station:
+    def __init__(self, naam, y, x, opties):
+        self.naam = naam
+        self.y = float(y)
+        self.x = float(x)
+        self.opties = opties
 
 class Stations:
-    def __init__(self, naam):
+    def __init__(self, naam, verbindingen):
         self.stations = self.laad_stations(naam)
         self.bezochte_stations = set()
+        self.verbindingen = verbindingen
+        self.vul_opties_in()
 
     def laad_stations(self, naam):
-        stations_list = set()
+        stations_list = []
         with open(naam, 'r', encoding='utf-8') as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
-                stations_list.add(row['station']) 
+                stations_list.append(Station(row['station'], row['y'], row['x']))
         return stations_list
     
+    def vul_opties_in(self):
+        station_dict = {station.naam: station for station in self._stations}
+        for verbinding in self.verbindingen:
+            if verbinding.station1 in station_dict:
+                station_dict[verbinding.station1].opties.append(verbinding.station2)
+            if verbinding.station2 in station_dict:
+                station_dict[verbinding.station2].opties.append(verbinding.station1)
 
     def is_bezocht(self, station):
         self.bezochte_stations.add(station)
